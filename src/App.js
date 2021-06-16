2f61b194f1745f191383aadf0e7d869d955b88a2
danilbek <deni200040@gmail.com>
2021-06-16 10:21:22 +0300

    todos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    let [todos, setTodos] = useState([
+        {text: "Купить бананы", completed: true},
+        {text: "Купить бананы2", completed: false}
+    ])
+    
+    const [text, setText] = useState("")
+
+    const deletedToDo = (number) =>{
+       const filtered = todos.filter((item, index) => {
+           if(index === number) {
+               return false
+           }
+             return true
+         } )
+         setTodos(filtered)    
+    }
+
+    const completed = (i) => {
+        const newTodos = todos.map((item,index) =>{
+            if(index === i) {
+                return {
+                    ...item,
+                    completed: !item.completed
+                }
+            }
+            return item
+        })
+        setTodos(newTodos)
+    }
+    
+
+    const Todos = todos.map((item,index) =>{
+        return <div className="todo">
+         <input  onClick={() => completed(index)} type="checkbox" checked={item.completed}></input>
+         <h3>{item.text}</h3>
+         <button onClick={()=> deletedToDo(index)}>X</button>
+         </div>
+     })
+     const addTodo = () =>{
+         todos.map((item, index) => {
+             console.log(text);
+             if(item.text !== text) {
+                setTodos([{
+                    text: text,
+                    completed: false
+                },
+                ...todos
+               ])
+             }
+            
+         })
+         
+        setText("")
+     }
+    return (
+        <div className="app">
+            <div className="header">
+               <h2>Список дел </h2> 
+            </div>
+            <hr />
+            <div className="form">
+                <input
+                value={text}
+                onChange={(e) => setText(e.target.value)}
+                 className="text" 
+                 placeholder="Введите текст..."/>
+                <button onClick={addTodo} className="button">Добавить</button>
+            </div>
+            <>
+            {Todos}
+            </>
+            
+        </div>
+    )
 }
 
-export default App;
+export default App
